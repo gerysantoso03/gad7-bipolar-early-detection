94cf0e9e5b261c9c012f7cf9834320af06d3c7bf
Oplet <60933839+gerysantoso03@users.noreply.github.com>
2021-11-14 22:40:29 +0700

    Gad 7 Form fix (#2)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
+
+import { useState } from 'react';
+import Gad7Form from '../components/home/Gad7Form';
 import HomeBody from '../components/HomeBody';
 import Navbar from '../components/Navbar';
 
 const Home = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div>
+      <button onClick={() => setShowModal(true)}>Show Modal</button>
+      {showModal ? <Gad7Form /> : null}
       <Navbar />
       <HomeBody />
     </div>
