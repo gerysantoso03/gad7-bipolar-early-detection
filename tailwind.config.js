30fc81832f10ed3d408ff96521d9e505adc7788a
gerysantoso03 <gerysantos03@gmail.com>
2021-11-23 00:03:39 +0700

    add mobile breakpoint on theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    screens: {
+      sm: { max: '540px' },
+    },
   },
   variants: {
     extend: {},
