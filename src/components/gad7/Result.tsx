40489a149555a0eb446ae5814b81eef63f7d65af
gerysantoso03 <gerysantos03@gmail.com>
2021-11-23 23:20:27 +0700

    Update responsive page styles

--- src/components/gad7/Result.tsx
+++ src/components/gad7/Result.tsx
@@ -12,7 +12,7 @@ const Result = ({ result, setResult }: resultType) => {
   return (
     <div className="result-modal fixed top-0 right-0 w-full h-full flex justify-center items-center">
       <div
-        className={`result-content ${show} p-4 relative flex flex-col justify-around items-center border w-3/5 h-3/5 bg-purple-100 rounded-md sm:w-4/5`}
+        className={`result-content ${show} py-12 px-4 relative flex flex-col justify-around items-center border w-3/5 bg-purple-100 rounded-md sm:w-4/5`}
       >
         <Link to="/">
           <button className="text-xs sm:text-md absolute top-3 left-2 hover:text-indigo-700 font-semibold text-indigo-500">
@@ -28,8 +28,10 @@ const Result = ({ result, setResult }: resultType) => {
         >
           X
         </button>
-        <h1 className="text-2xl font-bold text-indigo-800">{result.title}</h1>
-        <p className="text-md font-semibold text-indigo-700 text-justify leading-7 sm:leading-7 sm:text-sm">
+        <h1 className="text-2xl sm:text-xl font-bold text-indigo-800 mb-4">
+          {result.title}
+        </h1>
+        <p className="text-md font-semibold text-indigo-700 text-justify leading-7 sm:leading-7 mb-8 sm:mb-4 sm:text-sm">
           {result.desc}
         </p>
         <p className="text-xs text-justify text-indigo-700">
